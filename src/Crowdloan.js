c4c97f588a081ee6263c4d47634c398bbeb0fb36
anizeani <36300021+anizeani@users.noreply.github.com>
2021-07-07 18:14:01 +0200

    refactoring removing unnecessary stuff and renaming

--- src/Crowdloan.js
+++ src/Crowdloan.js
@@ -1,82 +1,66 @@
-import React from 'react';
-class Crowdloan extends React.Component {
-  constructor () {
-    super();
-    this.state = {
-      loading: true,
-      depositor: null,
-      verifier: null,
-      deposit: null,
-      raised: null,
-      end: null,
-      cap: null,
-      lastContribution: { ending: null },
-      firstPeriod: null,
-      lastPeriod: null,
-      trieIndex: null
-    };
-  }
+import React, { useState } from 'react';
+import { useSubstrate } from './substrate-lib';
+// to flatten nested JSON
+// var flatten = require('flat')
 
-  async componentDidMount () {
-    this.getItems();
-  }
+export default function Main (props) {
+    const { api } = useSubstrate();
+    const [loading, setLoading] = useState(true);
+    let [crowdLoan, setCrowdLoan] = useState({
+        // raised: null,
+        // end: null,
+        // cap: null,
+        // lastContribution: { preEnding: null },
+        // firstPeriod: null,
+        // lastPeriod: null,
+    });
 
-  kMFormatter (num) {
-    const decimals = api.registry.chainDecimals;
-    const numAfterDecimalReduction = Math.abs(num) / (Math.pow(10, decimals));
-    if (Math.abs(numAfterDecimalReduction) > 999 && Math.abs(numAfterDecimalReduction) < 999999) {
-      return Math.sign(numAfterDecimalReduction) * ((Math.abs(numAfterDecimalReduction) / 1000).toFixed(1)) + ' k';
-    } else if (Math.abs(numAfterDecimalReduction) > 999999) {
-      return Math.sign(numAfterDecimalReduction) * ((Math.abs(numAfterDecimalReduction) / 1000000).toFixed(1)) + ' M';
-    } else {
-      return Math.sign(num) * Math.abs(num);
-    }
-  }
 
-  async getItems () {
     const queryResHandler = result => {
-      const data = JSON.parse(result);
-      this.setState({
-        depositor: data.depositor,
-        verifier: data.verifier,
-        deposit: data.deposit,
-        raised: data.raised,
-        end: data.end,
-        cap: data.cap,
-        lastContribution: data.lastContribution,
-        firstPeriod: data.firstPeriod,
-        lastPeriod: data.lastPeriod,
-        triePeriod: data.triePeriod,
-        loading: false
-      });
+        const toHumanData = result.toHuman();
+        setCrowdLoan(crowdLoan = (toHumanData));
+        // to flatten nested JSON
+        // setcrowdLoan(crowdLoan = flatten(data));
+        setLoading(false);
     };
     const transformed = ['2004'];
     const palletRpc = 'crowdloan';
     const callable = 'funds';
-    await api.query[palletRpc][callable](...transformed, queryResHandler);
-  }
 
-  render () {
+    const getCrowdLoanData = async () => {
+        await api.query[palletRpc][callable](...transformed, queryResHandler);
+    };
+
+
+
+    if (Object.keys(crowdLoan).length == 0) {
+        getCrowdLoanData();
+    }
+
     return (
-            <div>
-                <h1>Crowdloan Funds</h1>
-                {this.state.loading || !this.state.depositor
-                  ? (
+        <div>
+            <h1>Crowdloan Funds</h1>
+            {loading
+                ? (
                     <div>loading...</div>
-                    )
-                  : (
+                )
+                : (
                     <div>
-                        <div>raised: {this.kMFormatter(parseInt(this.state.raised, 16))}KSM</div>
-                        <div>end: {this.state.end}</div>
-                        <div>cap: {this.kMFormatter(parseInt(this.state.cap, 16))}KSM</div>
-                        <div>last_contribution.ending: {this.state.lastContribution.ending}</div>
-                        <div>first_period: {this.state.firstPeriod}</div>
-                        <div>last_period: {this.state.lastPeriod}</div>
+                        {/* dynamic rendering of key, value pair of json: needs to be flattened! */}
+                        {/* {Object.keys(crowdLoan).map((key, i) => (
+                            <p key={i}>
+                                <span>{key}: </span>
+                                <span>{crowdLoan[key]}</span>
+                            </p>
+                        ))} */}
+                        <div>raised: {(crowdLoan.raised)}</div>
+                        <div>end: {crowdLoan.end}</div>
+                        <div>cap: {(crowdLoan.cap)}</div>
+                        <div>last_contribution.preEnding: {crowdLoan.lastContribution.PreEnding}</div>
+                        <div>first_period: {crowdLoan.firstPeriod}</div>
+                        <div>last_period: {crowdLoan.lastPeriod}</div>
                     </div>
-                    )}
-            </div >
+                )}
+        </div >
     );
-  }
 }
-
-export default Crowdloan;
