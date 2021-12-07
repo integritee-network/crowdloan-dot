d5814852947abff675eaea095a77bf8c47bceecc
Alain Brenzikofer <news@brenzi.ch>
2021-12-07 08:46:13 +0100

    set deadline

--- src/EarlySupporter.js
+++ src/EarlySupporter.js
@@ -19,7 +19,7 @@ export default function EarlySporter (props) {
                             It’s easy to follow the crowd, but some can see the future before everyone else.
                             </p>
                             <p>
-                                That’s why we are rewarding the early pioneers. Back us in the first 72h and receive a 20% Early Supporter Bonus.
+                                That’s why we are rewarding the early pioneers. Back us in the first 72h before Fri Dec 10th 2021 9:00 GMT and receive a 20% Early Supporter Bonus.
                                 This will be added to your standard Supporter Reward, which is proportional to your KSM contribution. The more you bond before the
                                 deadline, the more TEER tokens you will be eligible to receive.
                             </p>
