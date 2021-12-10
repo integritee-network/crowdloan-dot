4b5e1930ed8795af406dff48a89664c502831f4b
brenzi <brenzi@users.noreply.github.com>
2021-12-10 09:09:20 +0100

    double rewards untik 20kKSM (#99)

--- src/Rewards.js
+++ src/Rewards.js
@@ -14,8 +14,9 @@ export default function Rewards () {
                             <h2>Supporter Rewards</h2>
                             <p>
                               Integritee tokens will be distributed to KSM holders who support us in the Kusama parachain auctions.
-                              The quantity of TEER you receive as a supporter is proportional to the amount of KSM you lock until late December 2022.
-                              Each KSM will be rewarded with 10 TEER tokens.
+                              The quantity of TEER you receive as a supporter is proportional to the amount of KSM you lock until
+                              late December 2022. Each KSM will be rewarded with 20 TEER tokens until we until we hit 20,000 KSM.
+                              Thereafter each KSM will be rewarded with 10 TEER.
                             </p>
                             <div className="main">
                             <div className="text">
@@ -31,12 +32,18 @@ export default function Rewards () {
                               large number of tokens will be distributed.</p>
                             </div>
                             <div className="text">
-                            <strong>As a supporter, you receive:</strong>
+                            <strong>As a fast mover supporter, you receive:</strong>
                             <div className="cal-box other">
-                                1 KSM locked = 10 TEER + bonuses
+                                1 KSM locked = 20 TEER + bonuses
                             </div>
-                              <p>subject to linear vesting over 48 weeks</p>
                             </div>
+                              <div className="text">
+                                <strong>After reaching the threshold of 20,000 KSM raised:</strong>
+                                <div className="cal-box other">
+                                  1 KSM locked = 10 TEER + bonuses
+                                </div>
+                                <p>subject to linear vesting over 48 weeks</p>
+                              </div>
                             </div>
                         </Grid.Column>
                         </Grid.Row>
