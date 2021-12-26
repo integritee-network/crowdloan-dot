70717751de820f2653087ebe4a0c779906cd6ccb
anizeani <eranventura@gmail.com>
2021-12-25 20:44:34 -0600

    make backgoround appear nice with mobile and different screen sizes, never overlap graphic with text and also place the circles next to the text

--- src/SupporterReward.js
+++ src/SupporterReward.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './css/App.css';
 import { Container, Grid } from 'semantic-ui-react';
-import backgroundimage1 from './Images/Guaranteed2.png';
+import backgroundimage1 from './Images/Guaranteed1.png';
 export default function SupporterReward (props) {
   return (
         <div className="green-bg" id="Support" style={{
@@ -21,18 +21,14 @@ export default function SupporterReward (props) {
                                     <span style={{ color: '#70e6da', fontSize: '15px' }} >GUARANTEED REWARDS</span>
                                     <h2>With Guaranteed Rewards, <br/>Our Backers Can’t Lose</h2>
                                     <Grid.Column id="column3" style={{ width: '50%!important' }}>
-
                                         <p>
                                             In addition, we are now offering further guaranteed bonus rewards for Integritee contributors,
                                             regardless of the outcome of the slot auctions. They will be distributed to crowdloan contributors relative
                                             to their opportunity cost in terms of the amount of KSM they contribute,
                                             and the length of time this KSM is locked in. The locked-in time starts to be measured from the
                                             block that includes the contribution and finishes with the last block of the crowdloan campaign.
-
                                         </p>
-
                                         <span style={{ color: '#000065' }} >Here’s how it works:</span>
-
                                     </Grid.Column>
                                 </Grid.Row>
 
@@ -42,7 +38,7 @@ export default function SupporterReward (props) {
                             <Grid.Column id="column2" >
                                 <Grid.Row columns={1} >
 
-                                    <Grid.Column id="mobilesupport" style={{ bottom: '-5.188rem', position: 'absolute' }}>
+                                    <Grid.Column id="mobilesupport" className="visibledesktop" style={{ bottom: '-12.188rem', position: 'absolute' }}>
 
                                         <div className="">
                                             <span style={{ color: '#000065' }} > Scenario 1: ≥ 20,000 KSM bonded</span>
@@ -61,23 +57,23 @@ export default function SupporterReward (props) {
                             </Grid.Column>
 
                             <Grid.Column id="column2" >
-                                <Grid.Row columns={1} >
+                                    <Grid.Row columns={1} >
 
-                                    <Grid.Column id="mobilesupport" style={{ bottom: '9.188rem', position: 'absolute' }}>
+                                        <Grid.Column id="mobilesupport" style={{ bottom: '2rem', position: 'absolute' }}>
 
-                                        <div className="">
-                                            <span style={{ color: '#000065' }} >Scenario 2: ≥ 40,000 KSM bonded</span>
-                                            <span style={{ color: '#70e6da', fontSize: '15px' }} >40,000 TEER distributed to all contributors</span>
+                                            <div className="">
+                                                <span style={{ color: '#000065' }} >Scenario 2: ≥ 40,000 KSM bonded</span>
+                                                <span style={{ color: '#70e6da', fontSize: '15px' }} >40,000 TEER distributed to all contributors</span>
 
-                                            If Integritee doesn’t win an auction and the crowdloan ends
-                                            with 40,000 KSM bonded or more, 40,000 TEER will be
-                                            distributed to contributors.
+                                                If Integritee doesn’t win an auction and the crowdloan ends
+                                                with 40,000 KSM bonded or more, 40,000 TEER will be
+                                                distributed to contributors.
 
-                                        </div>
+                                            </div>
 
-                                    </Grid.Column >
+                                        </Grid.Column >
 
-                                </Grid.Row>
+                                    </Grid.Row>
 
                             </Grid.Column>
 
